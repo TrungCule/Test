1596babeae6e891c2f9768a319c0afe2f5456b57
TrungNguyen <trungnd@softdreams.vn>
2023-02-20 15:13:36 +0700

    upd

---
+++ src/main/java/com/example/TEST/domain/Employee.java
@@ -0,0 +1,74 @@
+package com.example.TEST.domain;
+
+import jakarta.persistence.*;
+
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employees")
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email_address", nullable = false)
+    private String emailId;
+
+    public Employee() {
+
+    }
+
+    public Employee(String firstName, String lastName, String emailId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailId = emailId;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", emailId=" + emailId
+                + "]";
+    }
+}
---
+++ src/main/java/com/example/TEST/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.TEST.repository;
+
+import com.example.TEST.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
